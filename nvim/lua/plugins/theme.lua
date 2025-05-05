1383c75a468021093b9c2a9e40c63c21f06f91ef
phuchoang2603 <xuanphuc.a1gv@gmail.com>
2025-05-05 12:07:51 -0400

    update neovim

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,9 +1,9 @@
 return {
-	{ "ellisonleao/gruvbox.nvim" },
+	{ "EdenEast/nightfox.nvim" },
 	{
 		"LazyVim/LazyVim",
 		opts = {
-			colorscheme = "gruvbox",
+			colorscheme = "nordfox",
 		},
 	},
 }
